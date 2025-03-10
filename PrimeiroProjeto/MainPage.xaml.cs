af48f4dcaccae1a26deb5a39eec423fe35c0d220
Ventura <ventura.py3@gmail.com>
2025-03-10 19:16:01 -0300

    feat: Add alerts and prompts

--- PrimeiroProjeto/MainPage.xaml.cs
+++ PrimeiroProjeto/MainPage.xaml.cs
@@ -15,7 +15,22 @@ namespace PrimeiroProjeto
         {
             await Shell.Current.GoToAsync("//Questoes");
         }
-       
+
+        private async void ShowAlert(object sender, EventArgs e)
+        {
+            bool resposta = await DisplayAlert("Continuar", "Gostaria de continuar", "Sim", "Não");
+        }
+
+        private async void ShowAlertWithOptions(object sender, EventArgs e)
+        {
+            string resposta = await DisplayActionSheet("Enviar para:", "Cancelar", "Deletar", "Facebook", "Twitter", "Instagram");
+        }
+
+        private async void ShowPrompt(object sender, EventArgs e)
+        {
+            string resposta = await DisplayPromptAsync("Opinião", "Diga sua opinião");
+        }
+
     }
 
 }
